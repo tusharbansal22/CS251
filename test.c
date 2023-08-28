9ec7dd4e978e5771f8039e0f1fb8dc6bf16e2307
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2023-08-28 22:44:51 +0530

    Assignment finished

---
+++ test.c
@@ -0,0 +1,22 @@
+// test.c
+#include <bits.stdc++.h>
+
+void main()
+{
+    int a, b, c;
+    a = 1;
+    b = 2.0;
+    c = a + b;
+    if (1 == 2)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf(i);
+    }
+}
